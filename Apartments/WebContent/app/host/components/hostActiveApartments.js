19ee94e02f4910453a79773002011705b642b5d5
Pufke <pufke1998@gmail.com>
2020-04-27 23:41:49 +0200

    Implemented InActiveDAO class

--- Apartments/WebContent/app/host/components/hostActiveApartments.js
+++ Apartments/WebContent/app/host/components/hostActiveApartments.js
@@ -1,7 +1,43 @@
 Vue.component("host-ActiveApartments",{
+    data(){
+        return { 
+            apartments: {}
+        }
+    },
+    
     template: `
-    <div>
-        <h1> Hello from host ActiveApartments </h1>
+    <div id = "styleForApartmentsView">
+
+        <ul>
+            <li v-for="apartment in apartments">
+                <h2> {{ apartment.typeOfApartment }} </h2>
+                <h2> {{ apartment.pricePerNight}} </h2>
+            </li>
+        </ul>
+        
+        <br>
+        <table border="1">
+        <tr bgcolor="lightgrey">
+            <th> Status </th><th> Type </th><th> Price </th><th> Rooms </th></tr>
+            <tr v-for="apartment in apartments">
+                <td> {{ apartment.status }} </td>
+                <td> {{ apartment.typeOfApartment }} </td>
+                <td> {{ apartment.pricePerNight}} </td>
+                <td> {{ apartment.numberOfRooms}} </td>
+            </tr>
+        </table>
+
     </div>
-    `
+    
+    `,
+    mounted(){
+        axios
+        .get('rest/apartments/getApartments')
+        .then( response=> {
+            this.apartments = response.data;	
+            console.log(response.data);
+        })
+    }
+
+
 });
