5a824119e3f3ef90b5d10d64a8a064121c88b7d2
vlaksi <daksrapsody@gmail.com>
2020-06-30 01:08:43 +0200

    [Administrator][Frontend] Search users by name, username, surname, role

--- Apartments/WebContent/app/administrator/components/administratorUsers.js
+++ Apartments/WebContent/app/administrator/components/administratorUsers.js
@@ -1,13 +1,12 @@
 Vue.component("administrator-users", {
     data() {
         return {
-            users: {},
-            searchData: {
-                userName: "",
-                password: "",
-                name: "",
-                surname: "",
-                role: ""
+            users: [],
+            searchField: {
+                userName: '',
+                name: '',
+                surname: '',
+                role: ''
             }
         }
     },
@@ -18,14 +17,10 @@ Vue.component("administrator-users", {
 
         <form method='post'>
 
-            <input type="text" v-model="searchData.userName" placeholder="username..." >
-            <input type="text" v-model="searchData.name" placeholder="Name..." >
-            <input type="text" v-model="searchData.surname" placeholder="surname..." >
-            <input type="text" v-model="searchData.role" placeholder="role..." >
-
-            <button type="button" @click="searchParam" >Search</button>
-            <button type="button" @click="cancelSearch">Cancel search</button>
-            
+            <input type="text" v-model="searchField.userName" placeholder="username..." >
+            <input type="text" v-model="searchField.name" placeholder="Name..." >
+            <input type="text" v-model="searchField.surname" placeholder="surname..." >
+            <input type="text" v-model="searchField.role" placeholder="role..." >            
 
         </form>
         <br>
@@ -40,7 +35,7 @@ Vue.component("administrator-users", {
                 <th> Role </th>
             </tr>
 
-            <tr v-for="user in users">
+            <tr v-for="user in filteredUsers">
                 <td> {{user.userName}}</td>
                 <td> {{user.password}}</td>
                 <td> {{user.name}} </td>
@@ -53,33 +48,38 @@ Vue.component("administrator-users", {
     </div>
     `,
     methods: {
-        searchParam: function (event) {
-            event.preventDefault();
+        isMatchSearch: function(user){
 
-            axios
-                .post('rest/users/getSearchedUsers', {
-                    "username": '' + this.searchData.userName,
-                    "password": '' + this.searchData.password,
-                    "name": '' + this.searchData.name,
-                    "surname": '' + this.searchData.surname,
-                    "role": '' + this.searchData.role
-                })
-                .then(response => {
-                    this.users = response.data;
-                })
+            // Check for username
+            if(! user.userName.match(this.searchField.userName))
+                return false;
+
+            // Check for name
+            if( ! user.name.match(this.searchField.name))
+                return false;
+
+            // Check for surname
+            if(! user.surname.match(this.searchField.surname))
+                return false;
+
+            // Check for role
+            if(! user.role.match(this.searchField.role))
+                return false;
+
+            // If i survive all if's now i am matched search
+            return true;
         },
-        cancelSearch: function () {
-            this.searchData.userName = "";
-            this.searchData.name = "";
-            this.searchData.surname = "";
-            this.searchData.role = "";
-
-            axios.get('rest/users/getJustUsers').then(response => (this.users = response.data));
-        }
 
     },
     mounted() {
         axios.get('rest/users/getJustUsers')
             .then(response => (this.users = response.data));
+    },
+    computed: {
+        filteredUsers: function () {
+            return this.users.filter((user) => {
+                return this.isMatchSearch(user);
+            });
+        }
     },
 });
