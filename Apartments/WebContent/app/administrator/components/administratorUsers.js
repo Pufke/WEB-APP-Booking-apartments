cde5e57003b662f87a5692fccd57df0f36c3b0e2
vlaksi <daksrapsody@gmail.com>
2020-07-02 01:42:47 +0200

    [Frontend][Administrator] Adding new host in app

--- Apartments/WebContent/app/administrator/components/administratorUsers.js
+++ Apartments/WebContent/app/administrator/components/administratorUsers.js
@@ -7,7 +7,9 @@ Vue.component("administrator-users", {
                 name: '',
                 surname: '',
                 role: ''
-            }
+            },
+            hideAddDialog: true,
+            newUser: {}
         }
     },
 
@@ -23,8 +25,9 @@ Vue.component("administrator-users", {
             <input type="text" v-model="searchField.role" placeholder="role..." >            
 
         </form>
-        <br>
 
+        <button type="button" @click="addNewHost()"> Add new host </button>
+        <br>
 
         <table border="1">
             <tr bgcolor="lightgrey">
@@ -50,9 +53,66 @@ Vue.component("administrator-users", {
         
         </table>
 
+
+
+
+        <!-- Modal DIALOG section for ADDING HOST -->
+        <div id = "addDialogForUsers" v-bind:class="{bgModal: hideAddDialog, bgModalShow: !hideAddDialog}">
+            <div class="modal-contents">
+        
+                <div class="close" @click="hideAddDialog = !hideAddDialog">+</div>
+
+                <form method='post'>
+                    
+                    <input type="text" v-model="newUser.userName" placeholder="Username" required>
+                    <input type="text" v-model="newUser.name" placeholder="Name" >
+                    <input type="text" v-model="newUser.surname" placeholder="Surname">
+                    <input type="password" v-model="newUser.password" placeholder="Password" required>
+                    <input type="password" placeholder="Re Password" required>
+
+                    <button type="button" @click="confirmAdding">Confirm</button>
+                    <button type="button" @click="hideAddDialog = !hideAddDialog">Cancel</button>
+
+                </form>
+
+            </div>
+        </div> <!-- End of modal adding dialog section -->
+
+
     </div>
     `,
     methods: {
+        addNewHost: function () {
+            this.hideAddDialog = !this.hideAddDialog;
+        },
+        confirmAdding: function () {
+            // warning/error if some fields are null or empty
+            // ref: https://stackoverflow.com/questions/5515310/is-there-a-standard-function-to-check-for-null-undefined-or-blank-variables-in
+            if (!this.newUser.userName || !this.newUser.password || !this.newUser.name || !this.newUser.surname) {
+                toastr["warning"]("All field is required", "Watch out !");
+                return;
+
+            }
+            axios
+                .post('rest/users/registration', {
+                    "username": this.newUser.userName,
+                    "password": this.newUser.password,
+                    "name": this.newUser.name,
+                    "surname": this.newUser.surname,
+                    "role": "HOST"
+                })
+                .then(response => {
+                    location.reload();
+                    toastr["success"]("Success created host, try and make one more !!", "Success registration!");
+                })
+                .catch(err => {
+                    console.log("\n\n ------- ERROR -------\n");
+                    console.log(err);
+                    toastr["error"]("We have alredy user with same username, try another one", "Fail");
+                    console.log("\n\n ----------------------\n\n");
+                })
+
+        },
         blockUser: function (userParam) {
 
             axios
@@ -107,8 +167,8 @@ Vue.component("administrator-users", {
 
     },
     mounted() {
-        axios.get('rest/users/getJustUsers')
-            .then(response => (this.users = response.data));
+        axios.get('rest/users/getJustUsers').then(response => (this.users = response.data));
+        axios.get('rest/users/getNewUser').then(response => (this.newUser = response.data));
     },
     computed: {
         filteredUsers: function () {
