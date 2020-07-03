73b381f1f24b4331bd867559f66deed3eb91ca8c
Pufke <pufke1998@gmail.com>
2020-07-03 17:06:31 +0200

    [LogoutService] Response status code implemented

--- Apartments/src/services/LogoutService.java
+++ Apartments/src/services/LogoutService.java
@@ -10,6 +10,8 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import beans.User;
+
 @Path("/logout")
 public class LogoutService {
 
@@ -22,11 +24,51 @@ public class LogoutService {
 	@Path("/someone")
 	@Produces(MediaType.TEXT_HTML)
 	public Response logoutUser() {
-		HttpSession session = request.getSession();
-		if(session != null && session.getAttribute("loginUser") != null) {
-			session.invalidate();
-		}
 		
-		return Response.ok().build();
+		if(isUserHost() || isUserAdmin() || isUserGuest()) {
+		
+			HttpSession session = request.getSession();
+			if(session != null && session.getAttribute("loginUser") != null) {
+				session.invalidate();
+			}
+			return Response
+					.status(Response.Status.ACCEPTED).entity("SUCCESS LOGOUT")
+					.build();
+		}
+		return Response.status(403).type("text/plain")
+				.entity("You do not have permission to access!").build();
+	}
+	
+	private boolean isUserHost() {
+		User user = (User) request.getSession().getAttribute("loginUser");
+		
+		if(user!= null) {
+			if(user.getRole().equals("HOST")) {	
+				return true;
+			}
+		}	
+		return false;
+	}
+	
+	private boolean isUserAdmin() {
+		User user = (User) request.getSession().getAttribute("loginUser");
+		
+		if(user!= null) {
+			if(user.getRole().equals("ADMINISTRATOR")) {
+				return true;
+			}
+		}	
+		return false;
+	}
+	
+	private boolean isUserGuest() {
+		User user = (User) request.getSession().getAttribute("loginUser");
+		
+		if(user!= null) {
+			if(user.getRole().equals("GUEST")) {
+				return true;
+			}
+		}	
+		return false;
 	}
 }
